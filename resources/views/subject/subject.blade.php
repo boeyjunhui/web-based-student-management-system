b00082e96b0c1eda80ac97a98f6ca454a2bb01a4
Boey Jun Hui <79003699+boeyjunhui@users.noreply.github.com>
2022-09-04 17:22:16 +0800

    v1.1

--- resources/views/subject/subject.blade.php
+++ resources/views/subject/subject.blade.php
@@ -24,7 +24,7 @@
                     <table class="table table-hover">
                         <thead>
                             <tr>
-                                <th>ID</th>
+                                <th>No</th>
                                 <th>Subject Name</th>
                                 <th>Subject Code</th>
                                 <th>Course Name</th>
@@ -32,16 +32,24 @@
                             </tr>
                         </thead>
                         <tbody id="table-data">
-                            @foreach ($subjects as $subject)
+                            @php $no = 1; @endphp
+
+                            @forelse ($subjects as $subject)
                                 <tr>
-                                    <td>{{ $subject->id}}</td>
+                                    <td>{{ $no }}</td>
                                     <td>{{ $subject->subjectName }}</td>
                                     <td>{{ $subject->subjectCode }}</td>
                                     <td>{{ $subject->courseName }}</td>
                                     <td><a class="btn btn-secondary secondary-btn" href={{ "subject/edit/".$subject->id }}><i class="far fa-edit"></i></a></td>
                                     <td><button class="btn btn-danger danger-btn delete-subject" value="{{ $subject->id }}"><i class="far fa-trash-alt"></i></button></td>
                                 </tr>
-                            @endforeach
+
+                                @php $no++; @endphp
+                            @empty
+                                <tr>
+                                    <td colspan="6">No Data Found</td>
+                                </tr>
+                            @endforelse
                         </tbody>
                     </table>
                 </div>
